4794b025370c9d5106d4740b88fedc07e398417c
ruroru <111705692+ruroru@users.noreply.github.com>
2025-10-04 20:53:44 +0000

    Stepping project version to prepare it for release

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ A templating engine for Clojure
 Add majavat to dependency list
 
 ```clojure
-[org.clojars.jj/majavat "1.11.1"]
+[org.clojars.jj/majavat "1.12.0"]
 ```
 
 ## Usage
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.jj/majavat "1.11.2-SNAPSHOT"
+(defproject org.clojars.jj/majavat "1.12.0"
   :description "Templating engine for clojure"
   :url "https://github.com/ruroru/majavat"
   :license {:name "EPL-2.0"
