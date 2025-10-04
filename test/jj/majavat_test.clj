8b97be1cacd283ecff2173a9ba7cfc880badadc1
jj <keistaslogairkosulys@gmail.com>
2025-10-04 15:13:12 +0300

    Add test to verify unexpected return-type

--- test/jj/majavat_test.clj
+++ test/jj/majavat_test.clj
@@ -77,6 +77,49 @@
                                                                            :sanitizer   (->Html)}) context)))))
           "verifying render to input stream"))))
 
+(deftest render-wrong-return-type-returns-string
+  (let [file-path "html/index.html"
+        context {
+                 :page-title      "Home - My Awesome Blog"
+                 :site-name       "My Awesome Blog"
+                 :nav-items       [
+                                   {
+                                    :url   "/"
+                                    :label "Home"
+                                    }
+                                   {
+                                    :url   "/about"
+                                    :label "About"
+                                    }
+                                   {
+                                    :url   "/contact"
+                                    :label "Contact"
+                                    }
+                                   ]
+                 :is-home-page    true
+                 :welcome-heading "Welcome to Our Blog!"
+                 :welcome-message "Stay up-to-date with our latest articles and insights."
+                 :posts           [
+                                   {
+                                    :url     "/posts/first-post"
+                                    :title   "Getting Started with Web Development"
+                                    :excerpt "A beginner's guide to the fundamentals of HTML, CSS, and JavaScript."
+                                    :date    "August 12, 2025"
+                                    }
+                                   {
+                                    :url     "/posts/second-post"
+                                    :title   "The Power of Templating Engines"
+                                    :excerpt "Learn how templating engines can streamline your web development workflow."
+                                    :date    "August 10, 2025"
+                                    }
+                                   ]
+                 :about-content   "We are a team of passionate developers dedicated to sharing knowledge about modern web technologies."
+                 :current-year    2025}]
+    (is (= (crlf->lf (slurp (io/resource "html/expected.html")))
+           (crlf->lf ((majavat/render file-path {:return-type :foo}) context))) "verifying wrong type returns string ")))
+
+
+
 
 (deftest try-rendering-file-that-does-not-exist
   (is (= (crlf->lf (slurp (io/resource "render-template-not-found.html")))
