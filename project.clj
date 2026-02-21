74fec23eb00b3d49f29f4ef3b35a533dad363a44
ruroru <111705692+ruroru@users.noreply.github.com>
2026-02-21 12:03:04 +0000

    Stepping project version to prepare it for release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.jj/majavat "1.20.1-SNAPSHOT"
+(defproject org.clojars.jj/majavat "1.20.1"
   :description "Templating engine for clojure"
   :url "https://github.com/ruroru/majavat"
   :license {:name "EPL-2.0"
